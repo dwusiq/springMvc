dd0b67da90d17253608e14890578a69d3fb3ffa5
dwusiq <dwusiq@163.com>
2017-06-01 14:56:20 +0800

    gradle、log4j2、springMvc、javaConfig、RequestMethod.get

---
+++ src/main/java/com/wusiq/controller/loginController.java
@@ -0,0 +1,22 @@
+package com.wusiq.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * 控制器
+ */
+@Controller
+@RequestMapping("loginController")
+public class loginController {
+    private Logger log = LoggerFactory.getLogger(loginController.class);
+
+    @RequestMapping(value = "/welcome.do",method = RequestMethod.GET)
+    public String doWelcome(){
+        log.info("login successed");
+        return "welcomePage";
+    }
+}
